1fa82ee38299478d8cb64b1c05b345338661ad7a
Lussandro Farias <167385164+Lussandrorj2@users.noreply.github.com>
2026-04-19 14:22:42 -0300

    Atualizar o models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -51,6 +51,8 @@ class Hotel(models.Model):
     subtitulo_hero_fr = models.CharField(max_length=255, blank=True)
 
     whatsapp  = models.CharField(max_length=20, blank=True, null=True)
+    lat       = models.FloatField(null=True, blank=True)  # ← adicionar
+    lng       = models.FloatField(null=True, blank=True)  # ← adicionar
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
