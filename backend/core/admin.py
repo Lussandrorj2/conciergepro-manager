7c82d8e4e789e39439fb2ddeb40d95a2f28c8798
Lussandro Farias <167385164+Lussandrorj2@users.noreply.github.com>
2026-04-27 17:15:57 -0300

    Atualizar o admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Hotel, Perfil, Passeio, ImagemPasseio
 
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ['user', 'hotel', 'cargo', 'ativo']
+    fields = ['user', 'hotel', 'cargo', 'ativo', 'nome_completo', 'telefone']
+
 admin.site.register(Hotel)
-admin.site.register(Perfil)
-admin.site.register(Passeio) 
-admin.site.register(ImagemPasseio) 
+admin.site.register(Passeio)
+admin.site.register(ImagemPasseio)
